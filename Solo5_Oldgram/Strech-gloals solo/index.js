4ab616a8b70860aa57260d298b5c2a883aabacbc
Rutucode <rutujapawarhere08@gmail.com>
2024-07-28 17:33:02 +0530

    solo-project

---
+++ Solo5_Oldgram/Strech-gloals solo/index.js
@@ -0,0 +1,46 @@
+const posts = [
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+  },
+];
+
+const likePost = document.getElementById("like-post");
+const likeBtn = document.getElementById("like-icon");
+const likeCount = document.getElementById("like-count");
+let count = 21;
+
+function likesCount() {
+  count += 1;
+  // likeBtn.innerHTML = count;
+  likeCount.textContent = `${count} likes`;
+}
+
+likeBtn.addEventListener("click", function () {
+  console.log("clicked");
+  this.classList.toggle("clicked");
+});
