e2f4b7498e48743b391a8057eb653e76144d0293
Rutucode <rutujapawarhere08@gmail.com>
2024-07-28 23:25:09 +0530

    essential css

--- Solo5_Oldgram/Strech-gloals solo/index.js
+++ Solo5_Oldgram/Strech-gloals solo/index.js
@@ -30,17 +30,42 @@ const posts = [
 ];
 
 const likePost = document.getElementById("like-post");
+
 const likeBtn = document.getElementById("like-icon");
+const likeBtn2 = document.getElementById("like-icon-2");
+const likeBtn3 = document.getElementById("like-icon-3");
+
 const likeCount = document.getElementById("like-count");
+const likeCount2 = document.getElementById("like-count-2");
+const likeCount3 = document.getElementById("like-count-3");
 let count = 21;
+let count2 = 12;
+let count3 = 98;
 
 function likesCount() {
   count += 1;
+  count2 += 1;
+  count3 += 1;
   // likeBtn.innerHTML = count;
   likeCount.textContent = `${count} likes`;
+  likeCount2.textContent = `${count2} likes`;
+  likeCount3.textContent = `${count3} likes`;
 }
 
 likeBtn.addEventListener("click", function () {
   console.log("clicked");
   this.classList.toggle("clicked");
+  likesCount();
+});
+
+likeBtn2.addEventListener("click", function () {
+  console.log("clicked");
+  this.classList.toggle("clicked");
+  likesCount();
+});
+
+likeBtn3.addEventListener("click", function () {
+  console.log("clicked");
+  this.classList.toggle("clicked");
+  likesCount();
 });
