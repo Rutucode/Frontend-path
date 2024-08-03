6fa2b35e8c32f07cedca8d195f6283c5e54261b0
Rutucode <rutujapawarhere08@gmail.com>
2024-08-03 23:00:32 +0530

    changes

--- 5 Essential Javascript/Meme Piker/index.js
+++ 5 Essential Javascript/Meme Piker/index.js
@@ -22,23 +22,36 @@
    in a <p> tag and then add them to the let you 
    created in step 2. 
 4. Render the string to the 'emotion-radios' div.
+Challenge:
+1. Add an eventListener to emotionRadios that will listen 
+   out for any *change* in our radio buttons. When it detects
+   a change, it should log out the id of the element that 
+   was selected.
+⚠️️ ️T️h️is won't work if the eventListener is listening out for a 
+   'click'. Google what event to listen for - I've already 
+   given you a clue!
 */
 import { catsData } from "./data.js";
 
-const emotionArry = [];
+const emotionRadios = document.getElementById("emotion-radios");
+
+emotionRadios.addEventListener("chnage", (e) => {
+  console.log(e.target.id);
+});
 
 function getEmotionsArray(cats) {
+  const emotionArry = [];
   for (let cat of catsData) {
     for (let emotion of cat.emotionTags) {
-      emotionArry.push(emotion);
+      if (!emotionArry.includes(emotion)) {
+        emotionArry.push(emotion);
+      }
     }
   }
   return emotionArry;
 }
 
 //getEmotionsArray(catData)
-
-const emotionRadios = document.getElementById("emotion-radios");
 
 function renderEmotionsRadios(cats) {
   const emotions = getEmotionsArray(cats);
