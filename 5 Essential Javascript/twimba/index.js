7a0af5c404f0b6e3aaedfc8a23f7cb50637c3f08
Rutucode <rutujapawarhere08@gmail.com>
2024-11-10 18:30:05 +0530

    change

--- 5 Essential Javascript/twimba/index.js
+++ 5 Essential Javascript/twimba/index.js
@@ -1,3 +1,19 @@
+
+/*
+Challenge:
+1. Replace the for of with a forEach.
+Challenge:
+1. Iterate over tweetsData and use the uuid 
+   saved in tweetId to identify the liked
+   tweet's object. Save that object to a 
+   new const called 'targetTweetObj'.
+⚠️ targetTweetObj should hold an object, NOT
+   an array.
+2. Increment targetTweetObj's 'likes' count 
+   by 1.
+3. Log out targetTweetObj.
+*/
+
 import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
@@ -6,15 +22,27 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
-function getFeedHtml(){
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+})
+
+function handleLikeClick(tweetId){
     
-/*
-Challenge:
-1. Replace the for of with a forEach.
-*/
-      
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    targetTweetObj.likes++
+    console.log(targetTweetObj)
+
+
+}
+
+function getFeedHtml(){
     let feedHtml = ``
-    for (let tweet of tweetsData){
+    
+    tweetsData.forEach(function(tweet){
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -24,12 +52,21 @@ Challenge:
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
                     ${tweet.retweets}
                 </span>
             </div>   
@@ -37,8 +74,13 @@ Challenge:
     </div>
 </div>
 `
-   }
+   })
    return feedHtml 
 }
 
-console.log(getFeedHtml())
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
