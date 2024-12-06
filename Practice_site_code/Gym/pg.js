73c2ff3696508a00afa44fd80cee212920530dd2
Rutucode <rutujapawarhere08@gmail.com>
2024-12-06 07:54:00 +0530

    changes

--- Practice_site_code/Gym/pg.js
+++ Practice_site_code/Gym/pg.js
@@ -1,12 +1,23 @@
 let count = 0;
-count += 1;
 
-totalCount = document.getElementById("daydCount");
+let totalCount = document.getElementById("day-Count");
 
 let increaseCount = document.getElementById("increment-btn");
 
 function increment() {
-    totalCount.innerHTML =count
-  increaseCount.innerHTML = totalCount + 1;
+  count += 1;
+  totalCount.innerHTML = count;
+  // increaseCount.innerHTML = totalCount + 1;
 }
 increment();
+
+/*
+let count = 0
+function increment(){
+  let  totalCount count +=1
+  or count +=1
+  console.log(totalCount)
+}
+
+increment()
+*/
